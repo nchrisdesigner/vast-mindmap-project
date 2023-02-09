c3bc6930858174b5a418390c6fd1faf9886bac5c
nchrisdesigner <66793672+nchrisdesigner@users.noreply.github.com>
2023-02-09 11:08:09 +0200

    Update Input.js

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,3 +1,24 @@
+// import React from 'react'
+// import styles from './input.module.css'
+// import {useForm} from 'react-hook-form'
+// import Error from '../InputError/Error'
+
+
+
+
+// function Input({id,type,register,label, errors}) {
+
+//   return (
+//     <div className={styles.inputContainer}>
+//         <label htmlFor={id}>{label}</label>
+//         <input {...register(id, {required:true, min: 0})} autoComplete='off' id={id} type={type} />
+//         <Error errors={errors} id={id} />
+//     </div>
+//   )
+// }
+
+// export default Input
+
 import React from 'react'
 import styles from './input.module.css'
 import {useForm} from 'react-hook-form'
@@ -6,12 +27,12 @@ import Error from '../InputError/Error'
 
 
 
-function Input({id,type,register,label, errors}) {
+function Input({id,type,register,label, errors, defvalue, iname}) {
 
   return (
     <div className={styles.inputContainer}>
         <label htmlFor={id}>{label}</label>
-        <input {...register(id, {required:true, min: 0})} autoComplete='off' id={id} type={type} />
+        <input {...register(id, {required:true, min: 0})} autoComplete='off' id={id} type={type} defaultValue={defvalue} name={iname} />
         <Error errors={errors} id={id} />
     </div>
   )
